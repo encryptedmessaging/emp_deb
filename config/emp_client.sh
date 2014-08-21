ecbec53b558f75c71535d143b6bbc20baa02e7e6
Ethan Gordon <ekgordon2@gmail.com>
2014-08-21 11:37:46 -0500

    Initial commit

---
+++ config/emp_client.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get Ports
+PORTS=$(sed -n 's/.*port *= *\([^ ]*.*\)/\1/p' < ~/.config/emp/msg.conf)
+IFS=' ' read -a array <<< $PORTS
+
+# Final Output
+xdg-open "http://localhost:${array[1]}" 2> /dev/null
